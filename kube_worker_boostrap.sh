0b97b72d3b638cf1e32939db6a065b96a8d7db6a
Junyoung <Kim_Junyoung@bah.com>
2019-06-07 10:11:16 -0700

    update ip, token, hash

--- kube_worker_boostrap.sh
+++ kube_worker_boostrap.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-sudo kubeadm join $some_ip:6443 --token $some_token --discovery-token-ca-cert-hash $some_hash
+#In Kubemaster node, output from $kubeadm token create --print-join-command 
+sudo kubeadm join 172.31.100.115:6443 --token 4mujoq.8ouwzx1di2kp2t5l --discovery-token-ca-cert-hash sha256:ed6443e1c492dfb638f251ece5663b348c3c0c6b3d1b82fc393d9707a1a3abb7
 #verify that all nodes have successfully joined the cluster
 kubectl get nodes

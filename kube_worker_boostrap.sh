737a3f08a561b00dd1a3cbdf661f98fd2ac26e76
Junyoung <Kim_Junyoung@bah.com>
2019-06-07 09:49:26 -0700

    update typeO

--- kube_worker_boostrap.sh
+++ kube_worker_boostrap.sh
@@ -1,4 +1,4 @@
-#! /bon/bash
+#! /bin/bash
 
 sudo kubeadm join $some_ip:6443 --token $some_token --discovery-token-ca-cert-hash $some_hash
 #verify that all nodes have successfully joined the cluster
